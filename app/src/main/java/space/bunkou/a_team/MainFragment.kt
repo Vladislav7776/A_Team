a20da1e608bf0ecf0f0cdb9de518adcb62fba050
Vladislav <vladislavpuskip@gmail.com>
2020-10-09 20:39:14 +0300

    NewFragment+Orientation

--- app/src/main/java/space/bunkou/a_team/MainFragment.kt
+++ app/src/main/java/space/bunkou/a_team/MainFragment.kt
@@ -5,22 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_login.*
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class Login : Fragment() {
+class MainFragment  : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
     }
 }

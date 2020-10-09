850a009b478d1b41ee150fa8d1f7cfb0d1bf67e6
sansaywm <sansaywm@gmail.com>
2020-10-09 21:36:08 +0300

    Fetch database add date

--- app/src/main/java/space/bunkou/a_team/MainActivity.kt
+++ app/src/main/java/space/bunkou/a_team/MainActivity.kt
@@ -2,10 +2,12 @@ package space.bunkou.a_team
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_authorization.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LayoutFIO
     }
 }

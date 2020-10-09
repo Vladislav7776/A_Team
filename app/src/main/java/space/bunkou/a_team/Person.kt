4671a3c0d3a27433972cea8e29cbf0afee633911
limitAlltheir <nikitakozkoz1999@gmail.com>
2020-10-09 19:15:08 +0300

    Create class for person.

---
+++ app/src/main/java/space/bunkou/a_team/Person.kt
@@ -0,0 +1,11 @@
+package space.bunkou.a_team
+
+data class Person(
+    val city: String,
+    val institution: String,
+    val organization: String,
+    val rank: String,
+    val region: String,
+    val user: String,
+    val data_of_birth: String
+)

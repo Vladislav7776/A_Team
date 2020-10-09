cad79b552e196f6756fb2ea24b5a100e37c020a7
Vladislav <vladislavpuskip@gmail.com>
2020-10-09 21:18:54 +0300

    NewFragment+Orientation

--- app/src/main/java/space/bunkou/a_team/MainFragment.kt
+++ app/src/main/java/space/bunkou/a_team/MainFragment.kt
@@ -5,18 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 
-class MainFragment  : Fragment() {
+class MainFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+
+
+        val adapter = PersonAdapter(personSet.addPersonToCollection())
+
+        personRecycler.adapter = adapter
+        personRecycler.layoutManager = LinearLayoutManager(this)
+        personRecycler.hasFixedSize()
 
     }
+}
 }

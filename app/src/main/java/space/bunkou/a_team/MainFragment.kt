8e26b1c202cc120e0877817e4c792872d1c99b36
Vladislav <vladislavpuskip@gmail.com>
2020-10-11 15:05:55 +0300

    add_AdMob_1

--- app/src/main/java/space/bunkou/a_team/MainFragment.kt
+++ app/src/main/java/space/bunkou/a_team/MainFragment.kt
@@ -1,17 +1,63 @@
 package space.bunkou.a_team
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
 import com.google.firebase.database.DatabaseReference
 import kotlinx.android.synthetic.main.fragment_main.*
 
+
 class MainFragment() : Fragment() {
+    lateinit var interstitialAd : InterstitialAd  // Реклама
+
+//    // Реклама - начало
+//    MobileAds.initialize(this, "ca-app-pub-4393452075947293~6502531964")
+//    interstitialAd = InterstitialAd(this)
+//    interstitialAd.setAdUnitId("ca-app-pub-4393452075947293/4582945634")
+//    var adRequest = AdRequest.Builder().build()
+//    interstitialAd.loadAd(adRequest)
+//    // Реклама - конец
+//
+//
+//    // Закрытие рекламы на крестик - начало
+//    interstitialAd.setAdListener(
+//     object : AdListener() {
+//        override fun onAdClosed() {
+//            try {
+//                val transition = null
+//                when (transition) {
+//                    0 -> {
+//                    }
+//                    1 -> {
+//                        val intent = Intent(this, Login::class.java)
+//                        startActivity(intent)
+//                    }
+//                    2 -> {
+//                        val intent1 = Intent(this, Authorization::class.java)
+//                        startActivity(intent1)
+//                    }
+//                    else -> {
+//                    }
+//                }
+//            } catch (e: Exception) {
+//                // пусто
+//            }
+//        }
+//    }
+//    // Закрытие рекламы на крестик - конец
+
+
     var mList = emptyList<Person>()
     override fun onCreateView(
         inflater: LayoutInflater,

6ca97d898a6e36de5fc4aaccf83dca8fb20f9574
sansaywm <sansaywm@gmail.com>
2020-10-13 01:45:35 +0300

    Merge branch 'master' of D:\A_Team with conflicts.

--- app/src/main/java/space/bunkou/a_team/Database.kt
+++ app/src/main/java/space/bunkou/a_team/Database.kt
@@ -1,5 +1,6 @@
 package space.bunkou.a_team
 
+import android.widget.EditText
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -34,10 +35,38 @@ fun addData(
     person.organization = organization.text.toString()
     person.organization2 = organization2.text.toString()
     person.region = region.text.toString()
-    REF_DATABASE_ROOT.child("омон").push().setValue(person)
+    REF_DATABASE_ROOT.child("omon").push().setValue(person)
 }
+
+fun addDataUser(login: EditText, password: EditText) {
+    val user = User()
+    user.name = login.text.toString()
+    user.password = password.text.toString()
+    user.isAuthorization = true
+    REF_DATABASE_ROOT.child("user").setValue(user)
+}
+
 // Функция преобразовывает полученые данные из Firebase в модель Person
 fun DataSnapshot.getPersonModel(): Person =
     this.getValue(Person::class.java) ?: Person()
 
+fun DataSnapshot.getUserModel(): User =
+    this.getValue(User::class.java) ?: User()
+
+fun getUserData(user: User) {
+    var mList = emptyList<User>()
+    val mUserListener: AppValueEventListener = AppValueEventListener { dataSnapshot ->
+        mList = dataSnapshot.children.map {
+            it.getUserModel()
+        }
+    }
+    if (mList.isNotEmpty()) {
+        for (k in mList) {
+            user.name = k.name
+            user.password = k.password
+            user.isAuthorization = k.isAuthorization
+        }
+    }
+    REF_DATABASE_ROOT.child("user").addValueEventListener(mUserListener)
+}
 
